2eaa5d11e1fdb2d9953e89ba4d06258efd2498b3
Ledio <L.Bode515@gmail.com>
2018-07-09 14:22:26 -0400

    first commit

---
+++ Library/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", content: "need bagels", user_id: 1)
+Book.create(title: "To Kill a Mockingbird", author: "Harper Lee", content: "need bagels", user_id: 1)
+Book.create(title: "	Cloudy With a Chance of Meatballs", author: "Judi Barrett", content: "need bagels", user_id: 1)
+Book.create(title: "I Was Told There'd Be Cake", author: "Sloane Crosley", content: "need bagels", user_id: 1)
+Book.create(title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", content: "need bagels", user_id: 1)
+Book.create(title: "The Hollow Chocolate Bunnies of the Apocalypse", author: "Robert Rankin", content: "need bagels", user_id: 1)
+Book.create(title: "Where the Wild Things Are", author: "Maurice Sendak", content: "need bagels", user_id: 1)
